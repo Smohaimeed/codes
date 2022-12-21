84db3afaf8b53c1fd424b34e8d30fe6d228b0ea5
Smohaimeed <121067114+Smohaimeed@users.noreply.github.com>
2022-12-22 02:01:59 +0300

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,23 @@
+#define Fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define ll long long
+#include <iostream>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <map>
+#include <cmath>
+
+using namespace std;
+ 
+ 
+ 
+
+int main(){
+ 
+    int x , y;
+    cin >> x >> y;
+    cout << x *y << "\n " ;
+    return 0;
+}
